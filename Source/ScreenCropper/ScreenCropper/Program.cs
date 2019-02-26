0a23fa0566ffc5bddd34b15f96407d2c731bafc9
Michael <mejevin@gmail.com>
2019-02-26 15:57:01 +0300

    Project preparation

---
+++ Source/ScreenCropper/ScreenCropper/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Screen Cropper Project - A lightweight screenshot taking program
+Copyright (C) 2019 MICHAEL NAIFIELD
+
+*/
+
+using System;
+using System.Windows.Forms;
+
+namespace ScreenCropper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ Source/ScreenCropper/ScreenCropper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Screen Cropper")]
+[assembly: AssemblyDescription("A lightweight screenshot taking program")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michael Naifield")]
+[assembly: AssemblyProduct("Screen Cropper")]
+[assembly: AssemblyCopyright("Copyright © 2010-2019 by Michael Naifield")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c37aeb6d-f298-4364-9d81-ba956dba5c7b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
