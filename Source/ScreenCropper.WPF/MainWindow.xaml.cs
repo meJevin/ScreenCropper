213652e86130dee3c1c379bbb79170536ac294de
Michael <mejevin@gmail.com>
2020-05-13 10:52:03 +0300

    add: full monitor size

--- Source/ScreenCropper.WPF/MainWindow.xaml.cs
+++ Source/ScreenCropper.WPF/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -28,6 +29,12 @@ namespace ScreenCropper.WPF
             InitializeComponent();
 
             HotkeyManager.Current.AddOrReplace("Test", Key.C, ModifierKeys.Control | ModifierKeys.Alt, TestHandler);
+
+            Width = SystemInformation.VirtualScreen.Width;
+            Height = SystemInformation.VirtualScreen.Height;
+
+            Left = 0;
+            Top = 0;
         }
 
         private void TestHandler(object sender, HotkeyEventArgs e)
