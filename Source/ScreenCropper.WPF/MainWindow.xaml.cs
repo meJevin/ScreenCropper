554d34d3697152476398289a9c539510f71f65af
Michael <mejevin@gmail.com>
2020-05-14 23:14:51 +0300

    feat: selection rectangle reacts to mouse events

--- Source/ScreenCropper.WPF/MainWindow.xaml.cs
+++ Source/ScreenCropper.WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -37,8 +38,10 @@ namespace ScreenCropper.WPF
             Top = 0;
         }
 
-        Point startPoint = new Point(-1, -1);
-        Point currPoint = new Point(-1, -1);
+        Point StartPoint = new Point(-1, -1);
+        Point CurrentPoint = new Point(-1, -1);
+
+        bool IsTakingScreenshot = false;
 
         private void ShortcutHandler(object sender, HotkeyEventArgs e)
         {
@@ -62,17 +65,29 @@ namespace ScreenCropper.WPF
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            IsTakingScreenshot = true;
 
+            StartPoint = Mouse.GetPosition(this);
+            CurrentPoint = StartPoint;
         }
 
         private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
+            if (IsTakingScreenshot)
+            {
+                CurrentPoint = Mouse.GetPosition(this);
 
+                Rect selectionRect = new Rect(StartPoint, CurrentPoint);
+
+                SelectionRectange.Margin = new Thickness(selectionRect.X, selectionRect.Y, 0, 0);
+                SelectionRectange.Width = selectionRect.Width;
+                SelectionRectange.Height = selectionRect.Height;
+            }
         }
 
         private void Window_MouseUp(object sender, MouseButtonEventArgs e)
         {
-
+            IsTakingScreenshot = false;
         }
     }
 }
