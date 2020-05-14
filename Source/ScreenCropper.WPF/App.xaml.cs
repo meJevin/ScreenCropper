291217ab74c20ea1213959b358e01bb089628479
Michael <mejevin@gmail.com>
2020-05-15 00:22:08 +0300

    change: remove startup URI and transfer shortcut logic to App class

--- Source/ScreenCropper.WPF/App.xaml.cs
+++ Source/ScreenCropper.WPF/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using NHotkey;
+using NHotkey.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ScreenCropper.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        ScreenshotOverlay Overlay = new ScreenshotOverlay();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            HotkeyManager.Current.AddOrReplace("Test", Key.C, ModifierKeys.Control | ModifierKeys.Alt, ShortcutHandler);
+        }
+
+        private void ShortcutHandler(object sender, HotkeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Hidden)
+            {
+                Overlay.Show();
+            }
+        }
     }
 }
